e467c744c45c781b0d783db75734cbf13c8e6a28
Gabrielly Ramos <ybagramos@gmail.com>
2021-11-26 15:36:31 -0300

    Criado models de Curriculum e Vagas.

--- Curriculum/models.py
+++ Curriculum/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from Pessoa.models import Pessoa
+from Vagas.models import Vagas
 
-# Create your models here.
+def path_curriculum(instance, filename):
+        # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+        return 'curriculo_{0}_{1}_{2}'.format(instance.pessoa.nome.split()[0],instance.pessoa.nome.split()[1], filename)
+
+class Curriculum(models.Model):
+    pessoa = models.ForeignKey(Pessoa,on_delete=models.CASCADE, null=True, blank=True, related_name="curriculos")
+    vaga = models.ForeignKey(Vagas,on_delete=models.CASCADE, null=True, blank=True, related_name="curriculos_vaga")
+    anexo = models.FileField(upload_to=path_curriculum)
--- Vagas/models.py
+++ Vagas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Vagas(models.Model):
+    titulo = models.CharField(max_length=50, null=True, blank=True)
+    descricao = models.CharField(max_length=200, null=True, blank=True)
+    requisitos = models.CharField(max_length=100, null=True, blank=True)
