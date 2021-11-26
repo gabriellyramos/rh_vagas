a9d8bd90ab1f5eacb0d79923f3c29300e33f2ae5
Gabrielly Ramos <ybagramos@gmail.com>
2021-11-26 15:44:38 -0300

    Adicionado campo de disponibilidade em vagas.

--- Vagas/models.py
+++ Vagas/models.py
@@ -4,3 +4,4 @@ class Vagas(models.Model):
     titulo = models.CharField(max_length=50, null=True, blank=True)
     descricao = models.CharField(max_length=200, null=True, blank=True)
     requisitos = models.CharField(max_length=100, null=True, blank=True)
+    disponivel = models.BooleanField(default=True)
